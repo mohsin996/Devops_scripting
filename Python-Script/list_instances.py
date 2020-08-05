4737d3c067869012ab45b69f33257e595b61fe3b
Mohsin Khan <mohsin_khan2@comcast.com>
2020-08-06 02:56:34 +0530

    update the comments

--- Python-Script/list_instances.py
+++ Python-Script/list_instances.py
@@ -2,23 +2,22 @@ import boto3
 import sys
 import os
 mylist = []
+
+#Checking with the default list to test list operation
 #mylist = ['t2.small', 't2.small','t2.medium','t2.medium','t2.large']
 
+#check if more than 2 arguments passed
 if len(sys.argv) > 2:
-    print ("usage: python list_instances.py 't2.medium'")
+    print ("usage only with one argument: python list_instances.py 't2.medium'")
     sys.exit(1)
 
-#region = os.environ.get('EC2REGION')
-
-#if region is None:
-#    print ("Please add a region or set the EC2REGION environment variable.")
-#    sys.exit(1)
-#region='us-east-2'
+#Boto Script to fetch details about EC2 with default region configured with aws configure
 ec2 = boto3.resource('ec2')
 for instance in ec2.instances.all():
     mylist.append(
             "{0}".format(instance.instance_type)
             )
+#condition based on the arguments passed
 if len(sys.argv) > 1:
     inst_type = sys.argv[1]
     for index, value in enumerate(mylist,start=1):
